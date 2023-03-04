d314fa3a20fba6f2181a9f3184f8ccaddb83e7e2
agogte <agogte@gmu.edu>
2023-03-04 18:30:19 -0500

    final commit

--- src/main/java/com/ltp/gradesubmission/GradeController.java
+++ src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -2,26 +2,53 @@ package com.ltp.gradesubmission;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Arrays;
+// import java.util.Arrays;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GradeController {
 
-    List<Grade> studentGrades = Arrays.asList(
-        new Grade("Harry", "Potions", "C-"), 
-        new Grade("Hermoine", "Bitchcraft", "A+"),
-        new Grade("Chad", "Charms", "A++")
-
-    );
+    List<Grade> studentGrades = new ArrayList<>();
 
     @GetMapping("/grades")
     public String getGrades(Model model){
         // Grade grade = new Grade("Harry", "Potions", "C-");
         model.addAttribute("grades", studentGrades);
         return "grades";
+    }
+
+    @GetMapping("/")
+    public String gradeForm(Model model, @RequestParam(required = false) String id){
+        Grade grade;
+        if (getGradeIndex(id) == -1000){
+            grade = new Grade();
+        } else {
+            grade = studentGrades.get(getGradeIndex(id));
+        }
+        model.addAttribute("grade", grade);
+        return "form";
+    }
+
+    @PostMapping("/handleSubmit")
+    public String submitForm(Grade grade){
+        if (getGradeIndex(grade.getId()) == -1000){
+            studentGrades.add(grade);
+        }
+        else {
+            studentGrades.set(getGradeIndex(grade.getId()), grade);
+        }
+        return "redirect:/grades";
+    }
+
+    public Integer getGradeIndex(String id){
+        for(int i = 0;i<studentGrades.size();i++){
+            if (studentGrades.get(i).getId().equals(id)) return i;
+        }
+        return -1000;
     }
 }
--- src/main/java/com/ltp/gradesubmission/GradeSubmissionApplication.java
+++ src/main/java/com/ltp/gradesubmission/GradeSubmissionApplication.java
@@ -2,12 +2,20 @@ package com.ltp.gradesubmission;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+// import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GradeSubmissionApplication {
+public class GradeSubmissionApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(GradeSubmissionApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(GradeSubmissionApplication.class);
 	}
 
 }
