7852b991ff18745833db4d51b3dc98cbea25dca4
agogte <agogte@gmu.edu>
2023-03-10 12:17:44 -0500

    added unit and integration tests

--- src/main/java/com/ltp/gradesubmission/repository/GradeRepository.java
+++ src/main/java/com/ltp/gradesubmission/repository/GradeRepository.java
@@ -3,7 +3,7 @@ package com.ltp.gradesubmission.repository;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.stereotype.Repository;
+// import org.springframework.stereotype.Repository;
 
 import com.ltp.gradesubmission.Grade;
 
